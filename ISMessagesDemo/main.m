81290ca5cf5bf73d5c21d6290ce6672887bcd9ce
Ilya Inyushin <trsaltn@yandex.ru>
2016-09-10 22:41:56 +0300

    Demo project.

---
+++ ISMessagesDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ISMessagesDemo
+//
+//  Created by Ilya Inyushin on 08.09.16.
+//  Copyright © 2016 Ilya Inyushin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
